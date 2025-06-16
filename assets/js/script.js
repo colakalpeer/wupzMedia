81c80df3fa89063a11ea7d9cd05185428f52e490
Alper Colak <colakalpeer93@gmail.com>
2025-06-16 09:22:40 +0300

    feat: I assigned the name data-parallax to the images I had previously loaded. then I was able to create the parallaxEffect with mouseMove values using addEventListener in JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,21 @@ const toggleNavbar = function () {
 
 
 addEventOnElement([navToggleBtn, overlay], "click", toggleNavbar);
+
+
+
+const parallaxElements = document.querySelectorAll("[data-parallax]");
+
+window.addEventListener("mousemove", event => {
+    for(let i = 0, len = parallaxElements.length; i < len; i++) {
+
+        const movementX = (event.clientX / window.innerWidth) * Number
+        (parallaxElements[i].dataset.parallaxSpeed);
+        const movementY = (event.clientY / window.innerHeight) * Number
+        (parallaxElements[i].dataset.parallaxSpeed);
+
+        parallaxElements[i].animate({
+            transform: `translate(${movementX}px, ${movementY}px)`
+    }, { duration: 500, fill: "forwards"});
+    }
+})

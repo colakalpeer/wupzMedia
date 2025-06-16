a7bb64317783a882c1e63dfc6ee46e2d38fe8c8c
Alper Colak <colakalpeer93@gmail.com>
2025-06-16 10:11:28 +0300

    feat: improvement made

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,40 @@
-'use strict';
-
+"use strict";
 
 const addEventOnElement = function (elements, eventType, callback) {
-    for (let i = 0, len = elements.length; i < len; i++) {
-        elements[i].addEventListener(eventType, callback);
-    }
-}
-
-
-
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
 
 const navbar = document.querySelector("[data-navbar]");
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
 const toggleNavbar = function () {
-    navbar.classList.toggle("active");
-    navToggleBtn.classList.toggle("active");
-    overlay.classList.toggle("active");
-    document.body.classList.toggle("nav-active");
-}
-
+  navbar.classList.toggle("active");
+  navToggleBtn.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
 
 addEventOnElement([navToggleBtn, overlay], "click", toggleNavbar);
 
-
-
 const parallaxElements = document.querySelectorAll("[data-parallax]");
 
-window.addEventListener("mousemove", event => {
-    for(let i = 0, len = parallaxElements.length; i < len; i++) {
+window.addEventListener("mousemove", (event) => {
+  for (let i = 0, len = parallaxElements.length; i < len; i++) {
+    const movementX =
+      (event.clientX / window.innerWidth) *
+      Number(parallaxElements[i].dataset.parallaxSpeed);
+    const movementY =
+      (event.clientY / window.innerHeight) *
+      Number(parallaxElements[i].dataset.parallaxSpeed);
 
-        const movementX = (event.clientX / window.innerWidth) * Number
-        (parallaxElements[i].dataset.parallaxSpeed);
-        const movementY = (event.clientY / window.innerHeight) * Number
-        (parallaxElements[i].dataset.parallaxSpeed);
-
-        parallaxElements[i].animate({
-            transform: `translate(${movementX}px, ${movementY}px)`
-    }, { duration: 500, fill: "forwards"});
-    }
-})
+    parallaxElements[i].animate(
+      {
+        transform: `translate(${movementX}px, ${movementY}px)`,
+      },
+      { duration: 500, fill: "forwards" }
+    );
+  }
+});
